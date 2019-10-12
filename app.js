cb1f0ac6343766da4057fe6362c9f914175660e6
Shreya Prasad <shreyapd06@gmail.com>
2019-10-12 16:50:57 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const bodyparser = require('body-parser')
-
+const path = require('path')
+const router = express.Router();
+let http = require('http');
+let fs = require('fs');
 let app= express();
 
-app.get('/', (req,res)=>
-    res.send('Homepage!')
-)
+router.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/static/index.html`);
+   });
+// route 1-> 
+// route2->
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
 
+
+app.use('/', router);
 const PORT=5000;
 
 app.listen(PORT,()=>console.log(`Server is running at port ${PORT}`))
