efff9ef645d4d50a513d79865a9de80f456c6127
Shreya Prasad <shreyapd06@gmail.com>
2019-10-12 16:08:29 +0530

    Add files via upload
    node.js server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+
+let app= express();
+
+app.get('/', (req,res)=>
+    res.send('Homepage!')
+)
+
+const PORT=5000;
+
+app.listen(PORT,()=>console.log(`Server is running at port ${PORT}`))
